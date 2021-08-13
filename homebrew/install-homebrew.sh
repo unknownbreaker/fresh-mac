037b8afb660cb23644af2adbce4ef132c1da4c76
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2021-08-13 13:40:01 -0400

    More changes

--- homebrew/install-homebrew.sh
+++ homebrew/install-homebrew.sh
@@ -2,12 +2,18 @@
 
 # Deprecated /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+if [ -e /usr/local/bin/brew ]
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  sudo chown -R `whoami`:admin /usr/local/bin
+  sudo chown -R `whoami`:admin /usr/local/share
+  sudo chown -R `whoami`:admin /usr/local/opt
+fi
+
 
 # source ../util/setBrewPrefix.sh
 
 # "${brewPrefix}/bin/brew" bundle install
-brew bundle install
+brew bundle 
 
 # sudo chmod -R 755 "${brewPrefix}/" # https://stackoverflow.com/questions/13762280/zsh-compinit-insecure-directories
 
