fc26bc592b9e6273d45fd4e1eff51ee6a98392d2
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2021-08-13 17:42:02 -0400

    Add then

--- home/setuphome.sh
+++ home/setuphome.sh
@@ -3,43 +3,43 @@
 cd ~/Documents
 echo "In $(pwd)"
 
-if [! -e Repos]
+if [! -e Repos]; then
   mkdir Repos
 fi
 
 cd Repos
 
-if [! -e Configs]
+if [! -e Configs]; then
   git clone https://github.com/unknownbreaker/Configs.git
 fi
 
 cd ~
 
-if [ -f .zshrc ]
+if [ -f .zshrc ]; then
   mv .zshrc .zshrc_old
   ln -s ~/Documents/Repos/Configs/Zsh/.zshrc .zshrc
 fi
-if [! -e .zshrc]
+if [! -e .zshrc]; then
   ln -s ~/Documents/Repos/Configs/Zsh/.zshrc .zshrc
 fi
 
-if [ -f .oh-my-zsh ]
+if [ -f .oh-my-zsh ]; then
   mv .oh-my-zsh .oh-my-zsh_old
   ln -s ~/Documents/Repos/Configs/Zsh/.oh-my-zsh .oh-my-zsh
 fi
-if [ ! -e .oh-my-zsh ]
+if [ ! -e .oh-my-zsh ]; then
   ln -s ~/Documents/Repos/Configs/Zsh/.oh-my-zsh .oh-my-zsh
 fi
 
-if [ ! -e .tmux ]
+if [ ! -e .tmux ]; then
   ln -s ~/Documents/Repos/Configs/Tmux/.tmux .tmux
 fi
-if [ ! -e .tmux.conf ]
+if [ ! -e .tmux.conf ]; then
   ln -s ~/Documents/Repos/Configs/Tmux/.tmux.conf .tmux.conf
 fi
 
 cd .config
-if [ ! -e karabiner ]
+if [ ! -e karabiner ]; then
   ln -s ~/Documents/Repos/Configs/Karabiner/ karabiner
 fi
   
