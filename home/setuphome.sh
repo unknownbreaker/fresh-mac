a054422e615747dd744d0d7bf1e0497363cc54ea
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2021-08-13 17:27:36 -0400

    Add stuff for home

---
+++ home/setuphome.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+cd ~/Documents
+
+if [-e Repos]
+  mkdir Repos
+fi
+
+cd Repos
+
+if [-e Configs]
+  git clone https://github.com/unknownbreaker/Configs.git
+fi
+
+cd ~
+
+if [ -f .zshrc ]
+  mv .zshrc .zshrc_old
+  ln -s ~/Documents/Repos/Configs/Zsh/.zshrc .zshrc
+fi
+if [-e .zshrc]
+  ln -s ~/Documents/Repos/Configs/Zsh/.zshrc .zshrc
+fi
+
+if [ -f .oh-my-zsh ]
+  mv .oh-my-zsh .oh-my-zsh_old
+  ln -s ~/Documents/Repos/Configs/Zsh/.oh-my-zsh .oh-my-zsh
+fi
+if [ -e .oh-my-zsh ]
+  ln -s ~/Documents/Repos/Configs/Zsh/.oh-my-zsh .oh-my-zsh
+fi
+
+if [ -e .tmux ]
+  ln -s ~/Documents/Repos/Configs/Tmux/.tmux .tmux
+fi
+if [ -e .tmux.conf ]
+  ln -s ~/Documents/Repos/Configs/Tmux/.tmux.conf .tmux.conf
+fi
+
+cd .config
+if [ -e karabiner ]
+  ln -s ~/Documents/Repos/Configs/Karabiner/ karabiner
+fi
+  
+cd ..
+
+
+
